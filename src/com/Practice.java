cce2b4151316be89e445e9618acb4094a04ed484
hyeon <einsight1@naver.com>
2022-10-04 16:33:26 +0900

    baby lion practice

---
+++ src/com/Practice.java
@@ -0,0 +1,11 @@
+package com;
+
+import java.sql.SQLOutput;
+
+public class Practice {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i += 1){
+            System.out.println(i);
+        }
+    }
+}
