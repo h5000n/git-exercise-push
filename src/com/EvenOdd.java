4de10fbcc218b960c7618adbbe6185e637d16037
hyeon <einsight1@naver.com>
2022-10-04 15:13:02 +0900

    code-up #1156 solved

--- src/com/EvenOdd.java
+++ src/com/EvenOdd.java
@@ -1,2 +1,13 @@
-package com;public class EvenOdd {
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+        if(num%2 ==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
 }
